ee13f1f68212ab19331dcca34b413bd1a693ca84
intanadhella <intanadhelia655@gmail.com>
2020-06-04 14:10:13 +0700

    add save function

--- src/App.js
+++ src/App.js
@@ -1,47 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import { Row, Col, InputGroup, Container, FormControl, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Row, Col, InputGroup, Container, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3000/todos')
-    .then((response) => {
-      console.log(response.data)
-      setTodos(response.data)
-    })
-  },[])
+    const todoList = JSON.parse(localStorage.getItem('todos')) || []
+    setTodos(todoList)
+  }, [])
+
+  const handleChange = (event) => {
+    let { value } = event.currentTarget;
+    setTodo(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const todoList = todos
+    todoList.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todoList))
+    setTodos([...todoList])
+    setTodo('')
+  };
 
   return (
     <Container>
       <Row className="my-5">
         <Col md={{ span: 4, offset: 4 }}>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Item"
-              aria-label="Item"
-            />
-            <InputGroup.Append>
-              <Button
-                variant="info"
-
-              >Submit</Button>
-            </InputGroup.Append>
-          </InputGroup>
+          <form onSubmit={handleSubmit}>
+            <InputGroup className="mb-3">
+              <Form.Control
+                type="text"
+                placeholder="Item"
+                aria-label="Item"
+                id="todo"
+                name="todo"
+                value={todo}
+                onChange={handleChange}
+                autoFocus
+              />
+              <InputGroup.Append>
+                <Button
+                  variant="info"
+                  type="submit"
+                >Submit</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </form>
         </Col>
       </Row>
       <Row>
         <Col md={{ span: 4, offset: 4 }}>
-        <h3>ToDo list: </h3>
-        <ul>
-          {
-            todos.map( (todo) => {
-              return( <li key={todo.id}> {todo.item} </li> )
-            })
-          }
-        </ul>
+          <h3>ToDo list: </h3>
+          <ul>
+            {
+              todos.map((todo, index) => {
+                return (
+                  <li key={index}>
+                    {todo}
+                  </li>
+                )
+              })
+            }
+          </ul>
         </Col>
       </Row>
     </Container>
